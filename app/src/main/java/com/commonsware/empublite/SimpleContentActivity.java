f21f099205ed752147daeb7ea99a62df584d5fdd
Guianu Leon <guianu.leon@gmail.com>
2014-07-20 15:50:55 +0300

    In onCreate(), we follow the standard recipe for defining our fragment if (and only if) we were started new, rather than restarted after a configuration change, by seeing if the fragment already exists. If we do need to add the fragment, we retrieve a string extra from the Intent used to launch us (identified as EXTRA_FILE), create an instance of SimpleContentFragment using that value from the extra, and execute a FragmentTransaction to add the SimpleContentFragment to the UI.

--- app/src/main/java/com/commonsware/empublite/SimpleContentActivity.java
+++ app/src/main/java/com/commonsware/empublite/SimpleContentActivity.java
@@ -1,9 +1,21 @@
 package com.commonsware.empublite;
 
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 
 
 public class SimpleContentActivity extends FragmentActivity {
 
+    public static final String EXTRA_FILE = "file";
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getSupportFragmentManager().findFragmentById(android.R.id.content) == null) {
+            String file = getIntent().getStringExtra(EXTRA_FILE);
+            Fragment f = SimpleContentFragment.newInstance(file);
+            getSupportFragmentManager().beginTransaction().add(android.R.id.content, f).commit();
+        }
+    }
 }
