f413521bfa5ef755498115f3a7eb6f03069f3524
Guianu Leon <guianu.leon@gmail.com>
2014-07-20 14:43:30 +0300

    Added pager and adapter | case R.id.about and R.id.help from onCreateOptionsMenu() now open SimpleContentActivity

--- app/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
+++ app/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
@@ -1,24 +1,35 @@
 package com.commonsware.empublite;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class EmPubLiteActivity extends FragmentActivity {
+
+    private ViewPager pager = null;
+    private ContentsAdapter adapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        pager = (ViewPager)findViewById(R.id.pager);
+        adapter = new ContentsAdapter(this);
+        pager.setAdapter(adapter);
+        findViewById(R.id.progressBar1).setVisibility(View.GONE);
+        findViewById(R.id.pager).setVisibility(View.VISIBLE);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.options, menu);
         return true;
@@ -33,11 +44,14 @@ public class EmPubLiteActivity extends FragmentActivity {
             case android.R.id.home:
                 return(true);
             case R.id.about:
+                Intent i = new Intent(this, SimpleContentActivity.class);
+                startActivity(i);
                 return(true);
             case R.id.help:
+                i = new Intent(this, SimpleContentActivity.class);
+                startActivity(i);
                 return(true);
         }
         return super.onOptionsItemSelected(item);
     }
-
 }
